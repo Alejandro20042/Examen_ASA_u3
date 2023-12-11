3267adffcc412f8412e657152708322cdd53a4eb
Alejandro20042 <alejandrosillasalcantar.com@gmail.com>
2023-12-11 09:20:12 -0700

    Agregar archivos de proyecto.

---
+++ ListaEnlazada.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_ASA
+{
+
+    class Productos
+    {
+        public int Precio { get; set; }
+        public string Nombre { get; set; }
+
+        public Productos(int precio, string nombre) 
+        { 
+            Precio = precio;
+            Nombre = nombre;
+        
+        }
+    }
+
+    class Nodo
+    {
+        public Productos Dato { get; set; }
+        public Nodo Siguiente { get; set; }
+    }
+
+
+    internal class ListaEnlazada
+    {
+        private Nodo cabeza { get; set; }
+
+        public void AgregarProducto(Productos producto)
+        {
+            Nodo nuevoNodo = new Nodo { Dato = producto, Siguiente = null };
+
+            if (cabeza == null)
+            {
+                cabeza = nuevoNodo;
+            }
+            else
+            {
+                Nodo actual = cabeza;
+                while (actual.Siguiente != null)
+                {
+                    actual = actual.Siguiente;
+                }
+                actual.Siguiente = nuevoNodo;
+            }
+
+        }
+
+        public int BuscarProductos(int precio)
+        {
+            Nodo actual = cabeza;
+            int posicion = 0;
+
+            while (actual != null)
+            {
+                if (actual.Dato.Precio == precio)
+                {
+                    Console.WriteLine($"El precio del producto:{actual.Dato.Precio}, con nombre: {actual.Dato.Nombre} esta en la posicion {posicion}");
+                    return posicion;
+
+                }
+                actual = actual.Siguiente;
+                posicion++;
+
+
+            }
+            return -1;
+
+        }
+
+
+
+        public void ImprimirProductos()
+        {
+            Nodo actual = cabeza;
+
+            while (actual != null)
+            {
+                Console.WriteLine($"Nombre del producto: {actual.Dato.Nombre}, Precio del producto: {actual.Dato.Precio} ");
+
+                actual = actual.Siguiente;
+            }
+
+        }
+
+        public void OrdenarProductos()
+        {
+            bool bandera = true;
+
+            while (bandera)
+            {
+                bandera = false;
+                Nodo actual = cabeza;
+                Nodo siguiente = cabeza.Siguiente;
+
+                while (siguiente != null)
+                {
+                    if(actual.Dato.Precio < actual.Dato.Precio)
+                    {
+
+                    }
+
+                }
+
+
+
+            }
+
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Examen_ASA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaEnlazada listaEnlazada = new ListaEnlazada();
+            while (true)
+            {
+
+
+                Console.WriteLine("1. Agregar Producto");
+                Console.WriteLine("2. Buscar Producto");
+                Console.WriteLine("3. Imprimir lista de Productos");
+                
+                int opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Ingresa el nombre del producto");
+                        string? nombre = Console.ReadLine();
+                        Console.WriteLine("Ingresa el precio del producto");
+                        int precio = int.Parse(Console.ReadLine());
+
+                        Productos productoNuevo = new Productos(precio, nombre);
+                        listaEnlazada.AgregarProducto(productoNuevo);
+
+                        break;
+                        case 2:
+                        Console.WriteLine("Dame el precio a buscar ");
+                        int precioBuscado = Convert.ToInt32(Console.ReadLine());
+
+                        listaEnlazada.BuscarProductos(precioBuscado);
+                        break;
+                        case 3:
+                        listaEnlazada.ImprimirProductos();
+                        break;
+
+                      
+
+                   
+                }
+
+
+            }
+
+        }
+    }
+}
